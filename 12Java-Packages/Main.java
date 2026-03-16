bd15228dbc5df7511e04ad1be3adc23f27339f1a
Raj Choudhary <raj.choudharycse2023@indoreinstitute.com>
2026-03-16 22:47:56 +0530

    Day 9

--- 12Java-Packages/Main.java
+++ 12Java-Packages/Main.java
@@ -1,3 +1,4 @@
+// import java.lang;  by default pakage
 import Tools.*;
 public class Main {
     public static void main(String[] args) {
@@ -7,3 +8,14 @@ public class Main {
         k.add(1, 7);
     }
 }
+
+/*
+
+Packages :-
+1. What is a Package in Java?
+A package in Java is a namespace that groups related classes and interfaces together.
+In simple words:
+Package = Folder that contains related Java classes
+Just like we organize files into folders on our computer, Java organizes classes into packages.
+
+*/
